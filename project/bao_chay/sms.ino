4d324d5db2e5aa226370352bc2fa63765a98cb84
asdcxsd <nguyenmanhthien98@gmail.com>
2018-12-06 13:51:13 +0700

    Update sms.ino

--- project/bao_chay/sms.ino
+++ project/bao_chay/sms.ino
@@ -12,10 +12,22 @@ float do_am = 0;
 void setup(){
     Serial.begin(115200);
     setup_sms(0);     // setup cho senset sim900a
-    setup_nhiet_do(); // setup cho senser nhiet do do am
-
+    //send_sms("0382132912", "nguyen manh thien");
+    //setup_nhiet_do("0382132912"); // setup cho senser nhiet do do am
+    String p = "";
+    kiem_tra_tai_khoan_goc(0);
+    kiem_tra_tai_khoan_km();
+    
+    
 }
 
-void loop() {
-   readSMS();
+void loop()
+{
+   String ans= readSMS(0);
+   if (ans != "error")
+   {
+    Serial.println("Tin nhan nhan duoc: \n" + ans);
+    send_sms("0382132912", ans);
+   }
+   //send_nhiet_do();
 }
